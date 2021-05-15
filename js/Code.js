21a480716599a30807746d62d0f07efbf8120157
Hailey-beep <72172315+Hailey-beep@users.noreply.github.com>
2021-05-15 15:35:58 -0400

    added files

---
+++ js/Code.js
@@ -0,0 +1,19 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,70)
+// add code for displaying rest of the names and the hints.
+    fill("white")
+    text("C U T N I F O N", 700, 145)
+    text("A T A D A S B A", 200, 375)
+    fill("lightblue")
+    text("Hint: Performs a specific task !!", 700, 165)
+    text("Hint: Stores important information in multiplayer games !!", 200, 400)
+}
